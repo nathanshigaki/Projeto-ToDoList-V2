024d74dceee82876fecf760aa6e6c987cd57de2c
Nathan <nathanshigaki@gmail.com>
2025-08-01 11:10:26 -0400

    Correção do problema CORS

--- src/main/java/br/com/nathanshigaki/v2/V2Application.java
+++ src/main/java/br/com/nathanshigaki/v2/V2Application.java
@@ -3,6 +3,10 @@ package br.com.nathanshigaki.v2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Defalt Server URL")})
 @SpringBootApplication
 public class V2Application {
 
